b750f43526cd2a63696572e82623eaa5da970369
Akshit kathuria <Akshit kathuria>
2017-02-18 11:49:14 +0530

    Add auto Scroll down of messages

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,21 @@
 var socket = io();
+
+function scrollToBottom () {
+    //selectorrs
+    //heights
+
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+        messages.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('Connected to server');
 });
@@ -25,6 +42,7 @@ socket.on('newMessage', function(message) {
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 })
 
 socket.on('newLocationMessage', function(message){
@@ -48,7 +66,7 @@ socket.on('newLocationMessage', function(message){
     });
 
     jQuery("#messages").append(html);
-
+    scrollToBottom();
 })
 
 
